276cb690db2b9fc537312123301a506d716cdd5a
wutfad23 <wutfad23@htl-kaindorf.at>
2026-03-17 11:12:45 +0100

    Create project structure and main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+# Math Magician Main File
+# usage: math operator operand1 operand2
+def main():
+    print("Welcome to Math Magician!")
+
+
+def main():
+    import sys
+
+    op = sys.argv[1]
+    a = float(sys.argv[2])
+    b = float(sys.argv[3])    
+
+if __name__ == "__main__":
+    main()
